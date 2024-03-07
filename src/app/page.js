b4352028a448e47c41bffb7da7b2b8923a9c33b4
Kodo Yousif <kodo.yousif@gmail.com>
2024-03-07 13:33:16 +0300

    suspensw

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react"
+import { useState, Suspense } from "react"
 
 import useData from "@/hooks/useData"
 import Category from "@/components/Category"
@@ -16,19 +16,21 @@ export default function Home() {
   const [_, { dir }] = useLanguage()
 
   return (
-    <main
-      dir={dir}
-      className="flex select-none min-h-screen flex-col items-center text-black"
-    >
-      <Languages />
+    <Suspense>
+      <main
+        dir={dir}
+        className="flex select-none min-h-screen flex-col items-center text-black"
+      >
+        <Languages />
 
-      {categories.map(({ items, label, key }) => (
-        <section className="w-full" key={key}>
-          <Category label={label} />
-          <ImageList setSelected={setSelected} items={items} />
-        </section>
-      ))}
-      <SidebarDialog item={selected} setSelected={setSelected} />
-    </main>
+        {categories.map(({ items, label, key }) => (
+          <section className="w-full" key={key}>
+            <Category label={label} />
+            <ImageList setSelected={setSelected} items={items} />
+          </section>
+        ))}
+        <SidebarDialog item={selected} setSelected={setSelected} />
+      </main>
+    </Suspense>
   )
 }
